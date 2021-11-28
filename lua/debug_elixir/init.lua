43f1c0a9bab81e2bec9f265d5613457838ad7f1e
Erik Bravo <ebz.erik@gmail.com>
2021-11-27 22:57:41 -0600

    module setup

--- lua/debug_elixir/init.lua
+++ lua/debug_elixir/init.lua
@@ -1,5 +1,6 @@
+M = {}
+
 local ts_utils = require 'nvim-treesitter.ts_utils'
-local ts_query = require"nvim-treesitter.query"
 
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --	Helper functions --
@@ -8,33 +9,53 @@ local function i(value)
     print(vim.inspect(value))
 end
 
-local function check_case(target_identifier, target)
-    local bufnm = vim.api.nvim_get_current_buf()
-
-    print("target identifier: ")
-    i(target_identifier)
-
-    if (target_identifier == "defmodule")  then
-        alias = ts_utils.get_node_text(target[2], bufnm)[1]
-
-        local ret = "#"..alias
-        print("Returning 'it's a defmodule' with alias: "..alias)
-        return ret
+local function coment_format(lang, coment, line)
+    if (lang == "elixir") then
+        local file_name = vim.fn.expand('%')
+        return "IO.puts( 'File: "..file_name.." - Line: "..line.." - "..coment.."' )"
     end
 
-    return "whatever"
+    return nil
 end
 
+local function insert_coment(row, col, coment, lang)
+    coment = coment_format(lang, coment, row)
+    coment = string.rep(" ", col)..coment -- identation
+    row = row + 1
+
+    vim.api.nvim_buf_set_lines(0, row, row, false, {coment})
+end
+
+local function get_query(lang)
+    if lang == "elixir" then
+        return vim.treesitter.parse_query(lang,[[
+        (call
+        target: (identifier) @function.identifier
+        (arguments (alias) @function.argument))
+
+        (call 
+        target: (identifier)
+        (arguments (call target: (identifier) @function.identifier))
+        )
+
+        (call
+        target: (identifier) @function.identifier (#eq? @function.identifier "if")
+        ) 
+            ]]
+        )
+    end
+
+    return nil
+end
 
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --	Main function--
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
-local function get_debug_msg()
+M.get_debug = function ()
     local bufnm = vim.api.nvim_get_current_buf()
     local lang = vim.bo.filetype
 
-    print("bufnm: "..bufnm.." lang: "..lang)
-
+    -- Parsing the fil
     local parser = vim.treesitter.get_parser(bufnm, lang)
     local tree = parser:parse()
     local root = tree[1]:root()
@@ -43,35 +64,17 @@ local function get_debug_msg()
     local node = ts_utils.get_node_at_cursor()
     local sRow, sCol, eRow, eCol = node:range()
 
-    print("sRow: "..sRow.." sCol: "..sCol.." eRow: "..eRow.."eCol: "..eCol)
-
-    local query = vim.treesitter.parse_query(lang,[[
-            (call
-                target: (identifier) @function.identifier
-                (arguments (alias) @function.argument))
-
-            (call 
-              target: (identifier)
-              (arguments (call target: (identifier) @function.identifier))
-            )
-
-            (call
-              target: (identifier) @function.identifier (#eq? @function.identifier "if")
-            ) 
-        ]]
-    )
+    local query = get_query(lang)
 
     local coment = ""
-    for id, capture, _ in query:iter_captures(root, bufnm) do
+    for id, capture, _ in query:iter_captures(root, bufnm, 0, sRow+1) do
         local node_text = ts_utils.get_node_text(capture, bufnm)
-        local name = query.captures[id] -- name of the capture in the query
+        -- local name = query.captures[id] -- name of the capture in the query
 
-        --[[ print("Capture name: "..name)
-        print("node text: "..node_text[1]) ]]
         coment = coment.."#"..node_text[1]
     end
 
-    print(coment)
+    insert_coment(sRow, sCol, coment, lang)
 end
 
-get_debug_msg()
+return M
